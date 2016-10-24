88d5e4c6f3a5331d1c69d27bc865e5c11f61f743
Justin Molineaux <justin.molineaux@gmail.com>
2016-10-24 12:27:09 -0400

    Dim all but hovered set.

--- js/resume.js
+++ js/resume.js
@@ -1,20 +1,20 @@
 $('.mugshot-pie, .resume-heatmap').on( 'mouseenter', '.dimmable-elem', function( event ) {
   var el    = $(event.target),
       skill = el.attr('data-skill');
-  $('.dimmable-elem[data-skill=' + skill + ']').attr('class', 'dimmable-elem highlight');
+  $('.dimmable-elem:not([data-skill=' + skill + '])').attr('class', 'dimmable-elem dim');
 });
 
 $('.mugshot-pie, .resume-heatmap').on( 'mouseleave', '.dimmable-elem', function( event ) {
   var el    = $(event.target),
       skill = el.attr('data-skill');
-  $('.dimmable-elem[data-skill=' + skill + ']').attr('class', 'dimmable-elem');
+  $('.dimmable-elem:not([data-skill=' + skill + '])').attr('class', 'dimmable-elem');
 });
 
 $('.experience-etc').on('mouseenter', '.employment-detail', function( event ) {
   var el  = $(event.target),
       job = el.attr('data-job');
   $('.dimmable-elem').attr('class', 'dimmable-elem');
-  $('.dimmable-elem[data-job=' + job + ']').attr('class', 'dimmable-elem highlight');
+  $('.dimmable-elem:not([data-job=' + job + '])').attr('class', 'dimmable-elem dim');
 });
 
 $('.experience-etc').on('mouseleave', '.employment-detail', function( event ) {
